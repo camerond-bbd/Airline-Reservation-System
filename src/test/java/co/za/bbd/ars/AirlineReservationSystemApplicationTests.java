0d28ae216bd8de973d34eb252d9c0d04424b9f16
Marius De Jager <marius.dejager2@standardbank.co.za>
2023-03-15 20:39:34 +0200

    Initial code

---
+++ src/test/java/co/za/bbd/ars/AirlineReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.bbd.ars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
