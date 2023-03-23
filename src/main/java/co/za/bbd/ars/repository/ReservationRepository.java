21a6aebb8b69004c1ae7cd722efd7f7f2be9240f
Joaquim-Chambuara <122440707+Joaquim-Chambuara@users.noreply.github.com>
2023-03-23 14:46:11 +0200

    Update ReservationRepository.java

--- src/main/java/co/za/bbd/ars/repository/ReservationRepository.java
+++ src/main/java/co/za/bbd/ars/repository/ReservationRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import co.za.ars.model.Reservation;
+import co.za.bbd.ars.model.Reservation;
 
 
 @Repository
