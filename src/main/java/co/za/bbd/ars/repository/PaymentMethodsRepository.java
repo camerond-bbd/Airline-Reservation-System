d614c3cef5349e567d5684ca125818a4dd0cf93d
Cameron <kyle.debastos@standardbank.co.za>
2023-03-23 09:05:51 +0200

    Added Jakarta to PaymentMethods, added Repository classes for Airline and PaymentMethod

---
+++ src/main/java/co/za/bbd/ars/repository/PaymentMethodsRepository.java
@@ -0,0 +1,11 @@
+package co.za.bbd.ars.repository;
+
+import co.za.bbd.ars.model.PaymentMethods;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface PaymentMethodsRepository extends JpaRepository<PaymentMethods, Integer> {
+    List<PaymentMethods> findAllByPaymentMethodName(String paymentMethodName);
+}
